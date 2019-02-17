d7066e9b8ab985ec872998724a91241440f1a27b
Fredrik G. Hovde <fghovde@gmail.com>
2019-02-17 23:16:21 +0100

    Level 1 process submit

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,14 @@
 //Use RegEx to validate form
+
+document.getElementById("submit").addEventListener("click", function () {
+    var inputFields = document.getElementsByTagName("input");
+    for (var i=0; i > inputFields.length; i++) {
+        var theInput = inputFields[i].value;
+        var thePattern = new RegExp(inputFields[i].pattern);
+        var theTest = thePattern.test(theInput);
+        
+        if(theTest === false){
+            console.log(inputFields[i].name + " is incorrect")
+        }
+    }
+})
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,18 @@
 //Console.log out elements in JSON file
+var video = [{
+    "id": 12312412312,
+    "name": "Ecuaciones Diferenciales",
+    "url": "/video/math/edo/12312412312",
+    "author": {
+        "data": [{
+            "name_author": "Alejandro Morales",
+            "url": "/author/alejandro-morales",
+            "type": "master"
+        }]
+    }
+}];
+
+for (var i = 0; i <= video.length; i++) {
+    var showElements = video[i];
+    console.log(showElements);
+};
